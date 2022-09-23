7d6f14a409fe1cd9dcc2380ad257dc28f761aa8e
JFmaia <jfmaia741@gmail.com>
2022-09-23 09:26:29 -0300

    fix: form base of screen.

--- loginCardInterative/src/App.tsx
+++ loginCardInterative/src/App.tsx
@@ -5,24 +5,22 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col justify-center items-center">
-      <div className="flex flex-row justify-around w-[100%] h-[100%] items-center">  
-        <span></span>
-        <div className="flex flex-col w-[440px] gap-2">
-          <div>
-            <span className="font-medium">CARDHOLDER NAME</span>
-            <input className="w-[100%] placeholder:text-slate-300 p-2 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
-          </div>
-          <div>
-            <span className="font-medium">CARD NAME</span>
-            <input className="w-[100%] placeholder:text-slate-300 p-2 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder=" e.g. 1234 5678 9123 0000"/>
-          </div>
-          <div>
-            <span className="font-medium">CARDHOLDER NAME</span>
-            <input className="w-[100%] placeholder:text-slate-300 p-3 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
-          </div>
-          <button></button>
+    <div className="max-w-[1344px] h-[100%] flex flex-row justify-between items-center self-center mx-40">  
+      <span></span>
+      <div className="flex flex-col w-[440px] gap-2">
+        <div>
+          <span className="font-medium text-[#21092F]">CARDHOLDER NAME</span>
+          <input className="w-[100%] placeholder:italic placeholder:#8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF  rounded-lg mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
         </div>
+        <div>
+          <span className="font-medium text-[#21092F]">CARD NAME</span>
+          <input className="w-[100%] placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder=" e.g. 1234 5678 9123 0000"/>
+        </div>
+        <div>
+          <span className="font-medium text-[#21092F]">CARDHOLDER NAME</span>
+          <input className="w-[100%] placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
+        </div>
+        <button className="w-[100%] rounded-lg bg-[#21092F] text-[#DEDDDF] p-4 mt-10 ">Confirm</button>
       </div>
     </div>
   )
