40cfbd13da88a40c4b82258c87df153aca43c12a
JFmaia <jfmaia741@gmail.com>
2022-09-22 21:52:11 -0300

    fix:settings for the form to work

--- loginCardInterative/src/App.tsx
+++ loginCardInterative/src/App.tsx
@@ -5,8 +5,25 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      
+    <div className="max-w-[1344px] mx-auto flex flex-col justify-center items-center">
+      <div className="flex flex-row justify-around w-[100%] h-[100%] items-center">  
+        <span></span>
+        <div className="flex flex-col w-[440px] gap-2">
+          <div>
+            <span className="font-medium">CARDHOLDER NAME</span>
+            <input className="w-[100%] placeholder:text-slate-300 p-2 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
+          </div>
+          <div>
+            <span className="font-medium">CARD NAME</span>
+            <input className="w-[100%] placeholder:text-slate-300 p-2 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder=" e.g. 1234 5678 9123 0000"/>
+          </div>
+          <div>
+            <span className="font-medium">CARDHOLDER NAME</span>
+            <input className="w-[100%] placeholder:text-slate-300 p-3 bg-white border border-slate-200 rounded-md mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
+          </div>
+          <button></button>
+        </div>
+      </div>
     </div>
   )
 }
