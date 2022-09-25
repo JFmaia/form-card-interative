28825bde9a138e1c9d8f674398a943961564bcc7
JFmaia <jfmaia741@gmail.com>
2022-09-25 09:13:28 -0300

    chore: Finalizing the form layout

--- loginCardInterative/src/App.tsx
+++ loginCardInterative/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <div className="max-w-[1344px] h-[100%] flex flex-row justify-between items-center self-center mx-40">  
-      <span></span>
+      <div>
+        
+      </div>
       <div className="flex flex-col w-[440px] gap-2">
         <div>
           <span className="font-medium text-[#21092F]">CARDHOLDER NAME</span>
@@ -14,11 +16,20 @@ function App() {
         </div>
         <div>
           <span className="font-medium text-[#21092F]">CARD NUMBER</span>
-          <input className="w-[100%] placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder=" e.g. 1234 5678 9123 0000"/>
+          <input className="w-[100%] placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder="e.g. 1234 5678 9123 0000"/>
         </div>
-        <div>
-          <span className="font-medium text-[#21092F]">CARDHOLDER NAME</span>
-          <input className="w-[100%] placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder="e.g. Jane Appleseed"/>
+        <div className="w-[100%] flex flex-row items-center">
+          <div className="w-[50%] flex flex-col">
+            <span className="font-medium text-[#21092F]">EXP. DATE (MM/YY)</span>
+            <div className="flex flex-row gap-1 items-center mt-1">
+              <input className="w-20 placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg" type="text" placeholder="MM"/>
+              <input className="w-20 placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg" type="text" placeholder="YY"/>
+            </div>
+          </div>
+          <div className="w-[50%] flex flex-col">
+            <span className="font-medium text-[#21092F]">CVC</span> 
+            <input className="placeholder:italic placeholder:##8E8593 p-3 bg-white text-[#21092F] border border-#DEDDDF rounded-lg mt-1" type="text" placeholder="e.g. 123"/>
+          </div>
         </div>
         <button className="w-[100%] rounded-lg bg-[#21092F] text-[#DEDDDF] p-4 mt-10 ">Confirm</button>
       </div>
